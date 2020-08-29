0a0e0c6960c0b453e00cbe91a6347a01d5c3a231
Kevin <giangcamvinh@gmail.com>
2020-08-28 21:54:25 -0700

    1st response

--- src/controllers/ChatBotController.js
+++ src/controllers/ChatBotController.js
@@ -1,3 +1,5 @@
+const request = require('request');
+
 const postWebhook = (req, res) => {
     let body = req.body;
 
@@ -11,6 +13,18 @@ const postWebhook = (req, res) => {
             // will only ever contain one message, so we get index 0
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
         });
 
         // Returns a '200 OK' response to all requests
@@ -45,6 +59,51 @@ const getWebhook = (req, res) => {
             res.sendStatus(403);
         }
     }
+}
+
+const handleMessage = (sender_psid, received_message) => {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
+};
+
+const callSendAPI = (sender_psid, response) => {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.FACEBOOK_PAGE_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
 }
 
 module.exports = {
